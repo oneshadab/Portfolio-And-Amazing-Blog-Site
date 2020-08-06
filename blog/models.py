f334014e0d3c61b070c82552ccf7a63ee15a2d54
Zenhar-Prodipto <zenhar.mz@gmail.com>
2020-08-06 20:16:37 +0600

    Changed logo & created UserBlog model

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User #User Model
 
 # Create your models here.
 
+#My Admin Model
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(default='DEFAULT VALUE')
     created_on = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+#Models for users
+
+class UserBlog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField ()
+    created_on = models.DateTimeField()
+    images = models.ImageField(upload_to="portfolio/images/",blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
